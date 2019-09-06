ecc0258af06f707d3dd6d46cb259e369b1160426
nagamalleswararaoj <nmrao.mtech@gmail.com>
2019-09-06 11:47:16 +0530

    initial commit

---
+++ src/main/java/com/springioc/examples/SpringIOCExamples/SpringIocExamplesApplication.java
@@ -0,0 +1,18 @@
+package com.springioc.examples.SpringIOCExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocExamplesApplication.class, args);
+		
+		System.out.println(" Hello world");
+		System.out.println("SpringIocExamplesApplication.main()");
+		
+		
+	}
+
+}
---
+++ src/test/java/com/springioc/examples/SpringIOCExamples/SpringIocExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springioc.examples.SpringIOCExamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringIocExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
